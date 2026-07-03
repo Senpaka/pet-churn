5a2dc251875314d461f17fbc6777237a4b973c13
Senpaka <egorkazakov01012007@mail.ru>
2026-07-04 00:49:35 +0300

    feat: create megration

--- core/paths.py
+++ core/paths.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parents[1]
 
 LOGS_DIR = BASE_DIR / "logs"
 MODELS_DIR = BASE_DIR / "models"
